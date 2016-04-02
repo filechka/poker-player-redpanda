46776fb3340579c2c2b1408c543377194f7a1104
dpilipenko@thumbtack.net <dpilipenko@thumbtack.net>
2016-04-02 12:59:51 +0600

    java

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,8 +8,12 @@ public class Player {
 
     static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonElement request) {
-        return 0;
+    public static int betRequest(JsonElement request)
+    {
+        Integer current_buy_in = request.getAsJsonObject().get("current_buy_in").getAsInt();
+        Integer player_in_action = request.getAsJsonObject().get("in_action").getAsInt();
+        return request.getAsJsonObject().get("current_buy_in").getAsInt() -
+                request.getAsJsonObject().get("players").getAsJsonArray().get(player_in_action).getAsJsonObject().get("bet").getAsInt();
     }
 
     public static void showdown(JsonElement game) {
