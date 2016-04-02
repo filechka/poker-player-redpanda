4afdcced031bcc093f552786c07b3a02be6d3196
PickRelated <itatarintsev@thumbtack.net>
2016-04-02 12:17:08 +0600

    Init strategy

--- src/main/kotlin/poker/Player.kt
+++ src/main/kotlin/poker/Player.kt
@@ -4,6 +4,9 @@ import org.json.JSONObject
 
 class Player {
     fun betRequest(game_state: JSONObject): Int {
+        if (Math.random() * 100 < 20) {
+          return Math.floor(JSONObject.players filter {it.name="RedPanda"} it.stack/50)
+        }
         return 0
     }
 
