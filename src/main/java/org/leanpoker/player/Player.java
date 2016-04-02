f4c1f5a143c97f8c9eb728732bb3c97faf2f7f9f
dpilipenko@thumbtack.net <dpilipenko@thumbtack.net>
2016-04-02 16:01:08 +0600

    upate

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,7 +10,7 @@ import java.util.Random;
 
 class PlayerLogic {
 
-    static final String VERSION = "Flame Princess 1.6";
+    static final String VERSION = "Flame Princess 1.7";
 
     public static int betRequest(JsonElement request)
     {
